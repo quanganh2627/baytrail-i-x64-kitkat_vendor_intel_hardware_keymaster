018757b447847f816cba83473110026b974bed53
Yong Yao <yong.yao@intel.com>
2014-10-09 06:42:30 +0800

    LOCAL_MODULE_PATH not supported in multiarch builds
    Change LOCAL_MODULE_PATH to LOCAL_MODULE_RELATIVE_PATH as expected by
    build/core/shared_library.mk:
    
    "LOCAL_MODULE_PATH for shared libraries is unsupported in multiarch
    builds, use LOCAL_MODULE_RELATIVE_PATH instead"
    
    Change-Id: If92aceeeea9315e0572672178974bd1c721e22a0
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-8086
    Signed-off-by: Yong Yao <yong.yao@intel.com>
    Signed-off-by: Robert Jarzmik <robert.jarzmik@intel.com>
    Reviewed-on: https://android.intel.com:443/291368

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := keystore.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := intel_keymaster.cpp
 LOCAL_C_INCLUDES := \
 	system/security/keystore \
